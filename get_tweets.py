4e0d888b0557b2983082dd934950f822088499c2
michhottinger <michhottinger@users.noreply.github.com>
2020-08-05 11:42:18 -0700

    updated info and made outline for plotly

--- get_tweets.py
+++ get_tweets.py
@@ -1,51 +1,10 @@
-
-"""
-Downloads all tweets from a given user.
-Uses twitter.Api.GetUserTimeline to retreive the last 3,200 tweets from a user.
-Twitter doesn't allow retreiving more tweets than this through the API, so we get
-as many as possible.
-t.py should contain the imported variables.
-"""
-
-from __future__ import print_function
-
-import json
-import sys
-
-import twitter
-from twitter_api import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
-
-
-def get_tweets(api=None, screen_name=None):
-    timeline = api.GetUserTimeline(screen_name=screen_name, count=200)
-    earliest_tweet = min(timeline, key=lambda x: x.id).id
-    print("getting tweets before:", earliest_tweet)
-
-    while True:
-        tweets = api.GetUserTimeline(
-            screen_name=screen_name, max_id=earliest_tweet, count=200
-        )
-        new_earliest = min(tweets, key=lambda x: x.id).id
-
-        if not tweets or new_earliest == earliest_tweet:
-            break
-        else:
-            earliest_tweet = new_earliest
-            print("getting tweets before:", earliest_tweet)
-            timeline += tweets
-
-    return timeline
+import urllib.parse
+from twitter_api import api
 
 
 if __name__ == "__main__":
-    api = twitter.Api(
-        CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
-    )
-    screen_name = sys.argv[1]
-    print(screen_name)
-    timeline = get_tweets(api=api, screen_name=screen_name)
-
-    with open('examples/timeline.json', 'w+') as f:
-        for tweet in timeline:
-            f.write(json.dumps(tweet._json))
-            f.write('\n')
+#     results = api.GetSearch(raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100")
+    q = "police chokehold"
+    results = api.GetSearch(raw_query="q=" + urllib.parse.quote(q) + "%20lang%3Aen%20since%3A2019-08-01")
+    for tweet in results:
+        print(tweet)
