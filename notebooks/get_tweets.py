6116a6ed672bc526bd7eab59ffd29f87ff5769e4
michhottinger <michhottinger@users.noreply.github.com>
2020-08-14 09:59:02 -0700

    FEAT/twitter_data added file to right place and added new keys back in locally

--- notebooks/get_tweets.py
+++ notebooks/get_tweets.py
@@ -1,1 +1,30 @@
 #code will go in here
+import urllib.parse
+import twitter
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+api = twitter.Api(consumer_key=os.getenv("CONSUMER_KEY"),
+                  consumer_secret=os.getenv("CONSUMER_SECRET"),
+                  access_token_key=os.getenv("ACCESS_TOKEN_KEY"),
+                  access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"),
+                  sleep_on_rate_limit=True)
+
+
+if __name__ == "__main__":
+
+#The index includes between 6-9 days of Tweets.
+
+
+
+    results = api.GetSearch(
+        term="(police chokehold) OR (pepperspray) OR (rubber bullets) OR (tazed) OR (teargas) OR (police assualt) OR (mace) OR (police shooting) OR (police hitting) OR (police attack)",
+        #geocode="-180,-90,180,90", need to find geocode for the USA???
+        since="2019-08-01",
+        lang="en",
+        total_count=100)
+    for tweet in results:
+        print(tweet)
+    print(len(results))
