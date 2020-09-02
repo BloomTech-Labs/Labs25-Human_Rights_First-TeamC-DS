17dab2db2faf240891a12426a7b6b753705da809
Axel <axacorro@gmail.com>
2020-09-01 21:12:58 -0700

    implemented post and get routes for incident and evidence

--- project/app/main.py
+++ project/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
@@ -6,7 +6,9 @@ from app.api import predict, viz
 from sqlalchemy.orm import Session
 from .database import SessionLocal, engine
 
-from . import models
+from . import crud, models, schemas
+
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -23,6 +25,27 @@ app = FastAPI(
     version='0.1',
     docs_url='/',
 )
+
+@app.post("/incidents/", response_model=schemas.Incidents)
+def create_incident(incident: schemas.Incidents, db: Session = Depends(get_db)):
+    db_incident = crud.get_incident(db, incident_id=incident.incident_id)
+    if db_incident:
+        raise HTTPException(status_code=400, detail='incident already exists')
+    return crud.create_incident(db=db, incident=incident)
+
+@app.get("/incidents/", response_model=List[schemas.Incidents])
+def read_incidents(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    incidents = crud.get_incidents(db, skip=skip,limit=limit)
+    return incidents
+
+@app.post('/incidents/{incident_id}/evidence/', response_model=schemas.Evidence)
+def create_evidence_for_incident(incident_id: str, evidence: schemas.Evidence, db: Session = Depends(get_db)):
+    return crud.create_incident_evidence(db, evidence=evidence, incident_id=incident_id)
+
+@app.get('/evidences/', response_model=List[schemas.Evidence])
+def read_evidences(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    evidences = crud.get_evidences(db, skip=skip, limit=limit)
+    return evidences
 
 app.include_router(predict.router)
 app.include_router(viz.router)
