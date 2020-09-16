df39a67c90ca1eed7a97e6f45e045032be37f37c
Axel <axacorro@gmail.com>
2020-09-16 13:16:27 -0700

    setups database connection

---
+++ project/app/database.py
@@ -0,0 +1,26 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = os.getenv("DB_URL")
+SQLALCHEMY_DATABASE_URL = DB_URL
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+Base = declarative_base()
