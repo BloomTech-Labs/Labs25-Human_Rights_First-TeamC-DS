fe1f74299b661d08a2287b5d46fd53408f847f35
maiali13 <60833374+maiali13@users.noreply.github.com>
2020-09-16 14:29:19 -0700

    feat/new_data_cron

--- project/app/utilities.py
+++ project/app/utilities.py
@@ -5,12 +5,29 @@ import geopy
 # Nominatim geocoding (built on OpenStreetMap data)
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
+# db
+from ..database import SessionLocal, engine
+from sqlalchemy.orm import Session
 
-# TODO DELETE AFTER FEATURE
-# police brutality data
-# pb_csv = pd.read_csv('https://raw.githubusercontent.com/2020PB/police-brutality/data_build/all-locations.csv')
-# pb_df = pd.DataFrame(pb_csv)
+router = APIRouter()
+con = engine.connect()
 
+
+def get_new(new_df):
+    '''
+    function that updates evidence for exsisting incidents
+    and gets new incidents
+    outputs df of new incidents and old incidents w/ new links column values
+    '''
+    # df of existing db data
+    db_df = pd.read_sql('SELECT * FROM "evidence_dim"', con)
+    # section of new data that MATCHES our database by id
+    match = new_df[new_df['id'].isin(db_df['id'])]
+    # set new df = NEW incident IDs aka incidents that were NOT in our db
+    new_df = new_df[~new_df['id'].isin(db_df['id'])]
+    ## TODO: append old incidents that had new evidence links to new_df
+    
+    return new_df
 
 def clean_pb2020(df):
     '''
@@ -36,6 +53,10 @@ def clean_pb2020(df):
     # remove leading and trailing whitespace from columns
     df['CITY'] = df['CITY'].str.strip()
     df['STATE_NAME'] = df['STATE_NAME'].str.strip()
+    # create column for state abbreviations
+    df['state_code'] = df['id'].str.split('-').str[0]
+    # ensure state code column is str and capitalized
+    df['state_code'] = df['state_code'].astype(str).str.upper()
     # put description column into str and convert text to lowercase
     df['text'] = df['text'].astype(str).str.lower()
     # regex for nlp:
