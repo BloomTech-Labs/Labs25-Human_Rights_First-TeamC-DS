e91fdfd3582b171f3cca1424efdbea5992392a03
Axel <axacorro@gmail.com>
2020-09-21 15:31:51 -0700

    feat. endpoint /incident/{tag} filters incidents by provided tag

--- project/app/main.py
+++ project/app/main.py
@@ -32,6 +32,16 @@ def read_incidents(skip: int = 0, limit: int = 100, db: Session = Depends(get_db
     incidents = crud.get_incidents(db, skip=skip, limit=limit)
     return incidents
 
+@app.get("/incidents/{tag}", response_model=List[schemas.Incidents])
+def incidents_by_tag(tag: str, db: Session = Depends(get_db)):
+    # query 'tags' table filter by tags.tag == tag
+    list_of_ids = db.query(models.Tags.incident_id).filter(models.Tags.tag == tag.capitalize()).all()
+    tag_incidents = []
+    for incident_id in list_of_ids:
+        incident = crud.get_incident(db,incident_id)
+        tag_incidents.append(incident)
+    return tag_incidents
+
 
 app.include_router(cron_update.router)
 app.include_router(predict.router)
