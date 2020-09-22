ac0f99d89bb74281682c4f3a74feaf2a764ca6e0
Vyraal1 <kevin.afable@gmail.com>
2020-09-22 11:34:39 -0400

    fix: lowercase washington and linting

--- project/app/utilities.py
+++ project/app/utilities.py
@@ -46,30 +46,32 @@ def clean_pb2020(df):
     df['CITY'] = df['CITY'].str.replace('DC', 'Washington', case=True)
     df['STATE_NAME'] = df['STATE_NAME'].str.replace(
         'Washington DC', 'District of Columbia', case=False)
-    df['CITY'] = df['CITY'].str.replace('Hungtington Beach', 'Huntington Beach', case=True)
+    df['CITY'] = df['CITY'].str.replace(
+        'Hungtington Beach', 'Huntington Beach', case=True)
     # fix id to match city name
-    df['id'] = df['id'].replace({'-dc': '-Washington'}, regex=True)
-    df['id'] = df['id'].replace({'-hungtingtonbeach': '-huntingtonbeach'}, regex=True)
+    df['id'] = df['id'].replace({'-dc': '-washington'}, regex=True)
+    df['id'] = df['id'].replace(
+        {'-hungtingtonbeach': '-huntingtonbeach'}, regex=True)
     df['id'] = df['id'].replace({'-costa-mesa': '-costamesa'}, regex=True
     # drop NaNs
     df.dropna(subset=['CITY', 'date'], inplace=True)
     # put date column in datetime
-    df['date'] = pd.to_datetime(df['date'], infer_datetime_format=True)
+    df['date']=pd.to_datetime(df['date'], infer_datetime_format=True)
     # remove leading and trailing whitespace from columns
-    df['CITY'] = df['CITY'].str.strip()
-    df['STATE_NAME'] = df['STATE_NAME'].str.strip()
+    df['CITY']=df['CITY'].str.strip()
+    df['STATE_NAME']=df['STATE_NAME'].str.strip()
     # create column for state abbreviations
-    df['state_code'] = df['id'].str.split('-').str[0]
+    df['state_code']=df['id'].str.split('-').str[0]
     # ensure state code column is str and capitalized
-    df['state_code'] = df['state_code'].astype(str).str.upper()
+    df['state_code']=df['state_code'].astype(str).str.upper()
     # put description column into str and convert text to lowercase
-    df['text'] = df['text'].astype(str).str.lower()
+    df['text']=df['text'].astype(str).str.lower()
     # regex for nlp:
     # remove backslash and apostrophe
-    df['text'] = df['text'].str.replace(r'\'', r'')
+    df['text']=df['text'].str.replace(r'\'', r'')
     # remove anything that isn't in a-z
-    df['CITY'] = df['CITY'].str.replace(r'[^a-zA-Z]', r' ')
-    df['text'] = df['text'].str.replace(r'[^a-zA-Z]', r' ')
+    df['CITY']=df['CITY'].str.replace(r'[^a-zA-Z]', r' ')
+    df['text']=df['text'].str.replace(r'[^a-zA-Z]', r' ')
 
     return df
 
@@ -78,18 +80,18 @@ def geoloc(df):
     '''
     Mutates a dataframe and adds geolocation data (lat/lon columns) based on city of incident
     '''
-    locator = Nominatim(user_agent="myGeocoder")
+    locator=Nominatim(user_agent="myGeocoder")
     # delay geocoding by 1 second between each incident to avoid rate limit
-    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
-    df['city_state'] = df['CITY'].astype(str) + ',' + df['STATE_NAME']
-    df['location'] = df['city_state'].apply(geocode)
+    geocode=RateLimiter(locator.geocode, min_delay_seconds=1)
+    df['city_state']=df['CITY'].astype(str) + ',' + df['STATE_NAME']
+    df['location']=df['city_state'].apply(geocode)
     # create lat, long, altitude as a single tuple column via pulling that data from location column
-    df['point'] = df['location'].apply(
+    df['point']=df['location'].apply(
         lambda loc: tuple(loc.point) if loc else None)
     # split latitude, longitude, and altitude columns into three separate columns
-    df[['latitude', 'longitude', 'altitude']] = pd.DataFrame(
+    df[['latitude', 'longitude', 'altitude']]=pd.DataFrame(
         df['point'].tolist(), index=df.index)
     # drop unnecessary columns
-    df = df.drop(['altitude', 'location', 'point', 'city_state'], axis=1)
+    df=df.drop(['altitude', 'location', 'point', 'city_state'], axis=1)
 
     return df
